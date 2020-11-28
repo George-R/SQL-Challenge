ae61aeb7bd7245adf64a5cce28d97654619defde
George Redak <george.redak91@gmail.com>
2020-11-28 16:08:33 -0500

    commit 2

--- SQL Challenge/queries.sql
+++ SQL Challenge/queries.sql
@@ -3,9 +3,11 @@ from employees
 join salaries
 	on employees.emp_no = salaries.emp_no;
 	
+	
 select first_name, last_name, hire_date
 from employees
 where hire_date LIKE '%1986';
+
 
 select departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
 from dept_manager
@@ -14,12 +16,14 @@ join departments
 join employees
 	on dept_manager.emp_no = employees.emp_no;
 	
+
 select dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
 from dept_emp
 join departments
 	on dept_emp.dept_no = departments.dept_no
 join employees
 	on dept_emp.emp_no = employees.emp_no;
+
 
 select first_name, last_name, sex
 from employees
@@ -45,6 +49,7 @@ join employees
 	on dept_emp.emp_no = employees.emp_no
 Where dept_name = 'Sales' or dept_name = 'Development';
 
+
 SELECT 
   employees.last_name, 
   COUNT(employees.last_name) as total
@@ -52,9 +57,3 @@ FROM employees
 GROUP BY employees.last_name
 ORDER BY employees.last_name
 
-From dept_manager, dept_emp
-join departments
-	on dept_emp.dept_no = departments.dept_no
-join employees
-	on dept_emp.emp_no = employees.emp_no
-Where dept_name = 'Sales';
