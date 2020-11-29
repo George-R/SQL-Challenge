b85183d41c2c275e394417c402a059bcedb66052
George Redak <george.redak91@gmail.com>
2020-11-28 19:50:39 -0500

    cleaner

--- SQL Challenge/queries.sql
+++ SQL Challenge/queries.sql
@@ -1,52 +1,52 @@
-select employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
-from employees
-join salaries
-	on employees.emp_no = salaries.emp_no;
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+JOIN salaries
+	ON employees.emp_no = salaries.emp_no;
 	
 	
-select first_name, last_name, hire_date
-from employees
-where hire_date LIKE '%1986';
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date LIKE '%1986';
 
 
-select departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
-from dept_manager
-join departments
-	on dept_manager.dept_no = departments.dept_no
-join employees
-	on dept_manager.emp_no = employees.emp_no;
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM dept_manager
+JOIN departments
+	ON dept_manager.dept_no = departments.dept_no
+JOIN employees
+	ON dept_manager.emp_no = employees.emp_no;
 	
 
-select dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
-from dept_emp
-join departments
-	on dept_emp.dept_no = departments.dept_no
-join employees
-	on dept_emp.emp_no = employees.emp_no;
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN departments
+	ON dept_emp.dept_no = departments.dept_no
+JOIN employees
+	ON dept_emp.emp_no = employees.emp_no;
 
 
-select first_name, last_name, sex
-from employees
-where first_name = 'Hercules'
-and 
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND 
 last_name like 'B%';
 
 
-select dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
-from dept_emp
-join departments
-	on dept_emp.dept_no = departments.dept_no
-join employees
-	on dept_emp.emp_no = employees.emp_no
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN departments
+	ON dept_emp.dept_no = departments.dept_no
+JOIN employees
+	ON dept_emp.emp_no = employees.emp_no
 Where dept_name = 'Sales';
 
 
-select dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
-from dept_emp
-join departments
-	on dept_emp.dept_no = departments.dept_no
-join employees
-	on dept_emp.emp_no = employees.emp_no
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN departments
+	ON dept_emp.dept_no = departments.dept_no
+JOIN employees
+	ON dept_emp.emp_no = employees.emp_no
 Where dept_name = 'Sales' or dept_name = 'Development';
 
 
